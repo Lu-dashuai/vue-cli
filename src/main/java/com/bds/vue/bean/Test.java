d27f0bb44f66bbd689b58e10578838ecc54982d3
kevin <386255178@qq.com>
2019-09-24 17:45:24 +0800

    提交测试

---
+++ src/main/java/com/bds/vue/bean/Test.java
@@ -0,0 +1,18 @@
+package com.bds.vue.bean;
+
+/**
+ * AUTHOR:Kevin Ding
+ * 2019/9/24
+ * test
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        String str1 = "hello";
+
+        str1 = "world";
+
+        System.out.println(str1);
+    }
+
+}
